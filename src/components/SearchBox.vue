e80d11a7c02743c41e7972a37c9685008998a2be
jicf <jicf@zylliondata.com>
2019-06-19 10:58:01 +0800

    second commit

---
+++ src/components/SearchBox.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="search-box">
+    <input type="text" placeholder="搜索" :value="value" @input="$emit('input', $event.target.value)">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "SearchBox",
+  props: ["value"],
+  mounted() {
+    console.log(this.value);
+  },
+  watch: {
+    value: function(newVal, oldVal) {
+      console.log(`newVal: ${newVal}`);
+    }
+  }
+};
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,17 +2,25 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <search-box v-model="searchText">
+      <span>slot</span>
+    </search-box>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
-  name: 'home',
+  name: "home",
   components: {
     HelloWorld
+  },
+  data() {
+    return {
+      searchText: ""
+    };
   }
-}
+};
 </script>
